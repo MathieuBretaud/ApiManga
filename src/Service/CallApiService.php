2a23f4690ff047a4093fddcb132b027eda3ba764
MathieuBretaud <mathieu.bretaud@gmail.com>
2022-06-28 14:02:37 +0200

    Add card and img

--- src/Service/CallApiService.php
+++ src/Service/CallApiService.php
@@ -13,11 +13,21 @@ class CallApiService
         $this->client = $client;
     }
 
-    public function getApi (): array
+    public function  getAllMange(): array
+    {
+        return $this->getApi('manga');
+    }
+
+    public function getAllAnime(): array
+    {
+        return $this->getApi('anime');
+    }
+
+    private function getApi (string $var): array
     {
         $response = $this->client->request(
             'GET',
-            'https://kitsu.io/api/edge/trending/anime'
+            'https://kitsu.io/api/edge/trending/' . $var
         );
         return $response->toArray();
     }
