6e99d7e264f7702ba5b4ef84266b8c9b5dc00e17
MathieuBretaud <mathieu.bretaud@gmail.com>
2022-07-19 17:14:50 +0200

    add mangaController and href cards

--- src/Controller/MangaController.php
+++ src/Controller/MangaController.php
@@ -11,20 +11,17 @@
 
 namespace App\Controller;
 
-use App\Service\CallApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AnimeController extends AbstractController
+class MangaController extends AbstractController
 {
-    #[Route('/anime/{id}', name: 'anime_show')]
-    public function show(CallApiService $callApiService, $id): Response
+    #[Route('/manga/{id}', name: 'manga_show')]
+    public function index(): Response
     {
-        $anime = $callApiService->getAllAnimeId($id);
-
-        return $this->render('anime/show.html.twig', [
-            'response' => $anime['data'],
+        return $this->render('manga/show.html.twig', [
+            'controller_name' => 'MangaController',
         ]);
     }
 }
