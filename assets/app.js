a57368ce9cded4bff6eba94304d04735d5403d0b
MathieuBretaud <mathieu.bretaud@gmail.com>
2022-07-26 14:40:18 +0200

    ADD loader

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,11 @@ import './styles/app.scss';
 import './bootstrap';
 
 import * as bootstrap from 'bootstrap';
+
+console.log('je suis dans app');
+const loader = document.querySelector('.loader');
+
+window.addEventListener('load', () => {
+    console.log('je suis dans loader');
+    loader.classList.add('fondu-out');
+})
